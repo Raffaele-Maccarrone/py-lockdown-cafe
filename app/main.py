4aabdc1155d4193d3f41f6406c903f7985c3cea7
Raffaele Maccarrone <raffaele.alves.maccarrone@gmail.com>
2026-04-30 22:57:52 -0300

    Solução

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from app.cafe import Cafe
-from app.errors import NotWearingMaskError, OutdatedVaccineError, NotVaccinatedError
+from app.errors import (
+    NotWearingMaskError,
+    OutdatedVaccineError,
+    NotVaccinatedError
+)
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
